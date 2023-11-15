57a4f9787645407d90942cdaab926b76e0a00bc4
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2023-11-15 10:46:54 -0400

    Update decrypt example

--- ubuntu/pbkdf2-example.sh
+++ ubuntu/pbkdf2-example.sh
@@ -4,7 +4,7 @@
 # Encrypt
 # echo "secret" | openssl enc -e -des3 -base64 -pbkdf2
 
-ENCRYPTED="U2FsdGVkX1+2Gdxrr/rZYJgo0g1pWDMsfTApPLUn1xLX2ZYz/sjVa8b0c2JYPnecTDwagOnV6apfZbyTjMfEt/iywxlLUmNmgXOeqD8D8Q=="
+ENCRYPTED="U2FsdGVkX1/QWW8OzrmP3JG0oG1PwDmSFTaPpluN1X03Kd9mBDYm5816sZzAphK/ZC7/V6D6QwD0Va9Ho9rhdV9ya6VGIVZcydC815Vi4dFCDFZUVOKCMCnV3XgV7gJMoSyZ3BZWHO/KF0txSmXB003/9ztd5iMCE2lNtnlg9CsocWHCJP/8EdnRM7cw2VtLZdNSyvxXPg=="
 
 # Decrypt
 echo "$ENCRYPTED" | openssl enc -d -des3 -base64 -pbkdf2
